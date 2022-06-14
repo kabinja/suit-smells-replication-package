fdf18d967b7a7ae800f07a9108d83de954a14a76
kabinja <renaudrwemalika@gmail.com>
2022-06-14 12:14:06 +0200

    download jar automatically and improve readme

--- initialize.py
+++ initialize.py
@@ -1,17 +1,33 @@
 import time
 import py7zr
+import urllib.request
+import os
 
 from os import listdir
 from os.path import isfile, join, dirname, realpath
 
-if __name__ == "__main__":
-    t_start = time.perf_counter()
-
+def unzip_data():
     files7z = [join(dirname(realpath(__file__)), join('data', f)) for f in listdir('data') if isfile(join('data', f)) and f.endswith('7z')]
 
     for file7z in files7z:
         with py7zr.SevenZipFile(file7z, 'r') as archive:
             archive.extractall(path='data')
+
+
+def download_jar():
+    jar_url = "https://github.com/serval-uni-lu/ikora-evolution/releases/download/ikora-evolution-0.1.8/ikora-evolution-0.1.8.jar"
+
+    if not os.path.exists('jar'):
+        os.makedirs('jar')
+
+    urllib.request.urlretrieve(jar_url, 'jar/ikora-evolution.jar')
+
+
+if __name__ == "__main__":
+    t_start = time.perf_counter()
+
+    unzip_data()
+    download_jar()
 
     t_stop = time.perf_counter()
     print("--------------------------------------------------")
--- run-smell-detector.sh
+++ run-smell-detector.sh
@@ -1,3 +1,3 @@
 find data/configurations -iname '*.json' | while read config; do
-    java -jar $1 -config $(readlink -f $config)
+    java -jar ./jar/ikora-evolution.jar -config $(readlink -f $config)
 done
