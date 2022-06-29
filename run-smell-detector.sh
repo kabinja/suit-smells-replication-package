738518fd64bba567c839d48cfeb5ec52e9e75412
kabinja <renaud.rwemalika@gmail.com>
2022-06-29 08:31:55 +0200

    update run smell script

--- run-smell-detector.sh
+++ run-smell-detector.sh
@@ -1,3 +1,7 @@
+BASEDIR=$(dirname "$0")
+
+mvn -Dmaven.repo.local=$BASEDIR/.jar dependency:get -Dartifact=lu.uni.serval:ikora-smells:0.1.0
+
 find data/configurations -iname '*.json' | while read config; do
-    java -jar $1 -config $(readlink -f $config)
+    java -jar $BASEDIR/lu/uni/serval/ -config $(readlink -f $config)
 done
