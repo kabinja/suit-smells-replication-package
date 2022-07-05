708a93dfac700b86999429506ea432cc6eb765c0
kabinja <renaudrwemalika@gmail.com>
2022-07-05 15:06:10 +0200

    download directly the jar from releases

--- run-smell-detector.sh
+++ run-smell-detector.sh
@@ -1,7 +1,5 @@
 BASEDIR=$(dirname "$0")
 
-mvn -Dmaven.repo.local=$BASEDIR/.jar dependency:get -Dartifact=lu.uni.serval:ikora-smells:0.1.0
-
 find data/configurations -iname '*.json' | while read config; do
     java -jar ./jar/ikora-evolution.jar -config $(readlink -f $config)
 done
