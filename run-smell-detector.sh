f8f757b4dbeb6f2365e86e95a31f92a4b076d2c6
kabinja <renaud.rwemalika@gmail.com>
2022-07-09 23:43:38 +0200

    fix run-smell

--- run-smell-detector.sh
+++ run-smell-detector.sh
@@ -6,6 +6,6 @@ if [ ! -d "./jar" ]; then
     wget $jar_url -O ./jar/ikora-evolution.jar
 fi
 
-#find data/configurations -iname '*.json' | while read config; do
-#    java -jar ./jar/ikora-evolution.jar -config $(readlink -f $config)
-#done
+find data/configurations -iname '*.json' | while read config; do
+    java -jar ./jar/ikora-evolution.jar -config $(readlink -f $config)
+done
