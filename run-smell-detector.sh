b72ef58586a0c23705267d034795b562f7bb661a
kabinja <renaud.rwemalika@gmail.com>
2022-07-09 23:42:52 +0200

    update ikora evolution version

--- run-smell-detector.sh
+++ run-smell-detector.sh
@@ -1,4 +1,4 @@
-version=0.1.9
+version=0.1.10
 jar_url="https://github.com/serval-uni-lu/ikora-evolution/releases/download/ikora-evolution-$version/ikora-evolution-$version.jar"
 
 if [ ! -d "./jar" ]; then
@@ -6,6 +6,6 @@ if [ ! -d "./jar" ]; then
     wget $jar_url -O ./jar/ikora-evolution.jar
 fi
 
-find data/configurations -iname '*.json' | while read config; do
-    java -jar ./jar/ikora-evolution.jar -config $(readlink -f $config)
-done
+#find data/configurations -iname '*.json' | while read config; do
+#    java -jar ./jar/ikora-evolution.jar -config $(readlink -f $config)
+#done
